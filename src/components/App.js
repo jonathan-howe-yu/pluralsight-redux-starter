131737ed52811305ad529cb6786e7953e70e5aee
Jon <jonathan.howe.yu@gmail.com>
2017-05-24 20:29:35 -0400

    Finished Async Status and Error Handling module

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 // This component handles the App template used on every page
 import React, {PropTypes} from 'react';
 import Header from './common/Header';
+import {connect} from 'react-redux';
 
 class App extends React.Component {
     render() {
         return (
             <div className="container-fluid">
-                <Header/>
+                <Header
+                    loading={this.props.loading}
+                />
                 {this.props.children}
             </div>
         );
@@ -17,4 +20,9 @@ App.propTypes = {
     children: PropTypes.object.isRequired
 };
 
-export default App;
+function mapStateToProps(state, onProps) {
+    return {
+        loading: state.ajaxCallsInProgress > 0
+    };
+}
+export default connect(mapStateToProps)(App);
